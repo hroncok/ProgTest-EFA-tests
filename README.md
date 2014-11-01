aa7044fade816fe9f1b6ceb0c4823fecc20bbf77
Miroslav Kravec <kravec.miroslav@gmail.com>
2014-11-01 12:19:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+usage: 
+```bash
+sh ${tests_folder}/tester.sh ./executable ${tests_folder}/matrioska
+```
---
+++ tester.sh
@@ -0,0 +1,6 @@
+for test in `ls $2| grep test | grep in | cut -d'.' -f1`;
+do
+    echo Test: $test
+    $1 < $2/${test}.in| diff - $2/${test}.out
+done
+read
